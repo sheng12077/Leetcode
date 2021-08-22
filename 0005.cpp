ab7f8421e34f24debf00ffc0702f43e909edd1a8
sheng <86102390+sheng120777@users.noreply.github.com>
2021-08-22 19:19:50 +0800

    Update 0005.cpp

--- 0005.cpp
+++ 0005.cpp
@@ -12,7 +12,7 @@ public:
         int len = 0;                                //len:最佳長度
         int start = 0;                              //start:起始點
         for (int i=0;i<n;i++){
-            int cur=max(getlen(i,i),getlen(i,i+1));//getLen(i,i)是奇數,getLen(i,i+1)是偶數
+            int cur=max(getlen(i,i),getlen(i,i+1)); //getlen(i,i)是奇數,getLen(i,i+1)是偶數
             if (cur>len){
                 len=cur;                            //比當前的最優解還要長就更新最優解
                 start=i-(len-1)/2;                  //len-1:因為偶數情況下length是2，起始點其實還是i
